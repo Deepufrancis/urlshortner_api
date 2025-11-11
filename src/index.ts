22019d6ba32de597093d8bee5424c2dabe56b3b7
Deepufrancis <deepufrancis999@gmail.com>
2025-11-12 02:08:43 +0530

    Fix CORS and update Swagger server URL

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./config/swagger";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || "https://urlshortner-api.onrender.com";
 
 // Connect Database
 connectDB();
